7f47e0ec2502c38f3f0be869b765702d3c8b90bd
Egor Ovechkin <egorka1391@gmail.com>
2019-06-06 13:24:25 +0500

    feature(i18n):add nested argument and string interpolation
    [MAJOR] Добавит возможность доставать строки из вложенных объектов.
    Уровни вложенности разделяются точкой (.), поэтому для
    перехода на новую версию необходимо заменить все ключи локалей, содержащие
    точку, или положить соответствующие локали во вложенные объекты.
    Например 'unique.locale.string.name'заменить на 'unique_locale_string_name'
    Добавит возможность интерполировать параметры строки заданные через %{param}.
    Пример {{ i18n 'unique.locale.string.name' true email="some@mail.ru" }}

--- test/i18n.js/helper.js
+++ test/i18n.js/helper.js
@@ -1,9 +1,12 @@
 let backupWindow;
 
-const 
+const
   TEST_LOCALE_VALUE = `test locale string`,
-  TEST_LOCALE_KEY   = `test.locale.key`,
-  TEST_LOCALE_ERROR = `translation missing app.i18n["${TEST_LOCALE_KEY}"]`;
+  TEST_LOCALE_INTERPOLATED_PARAM = '%{param}',
+  TEST_LOCALE_KEY   = `test_locale_key`,
+  TEST_LOCALE_NESTED_KEY   = `test.locale.key`,
+  TEST_LOCALE_ERROR = `translation missing app.i18n["${TEST_LOCALE_KEY}"]`,
+  TEST_LOCALE_NESTED_ERROR = `translation missing app.i18n["${TEST_LOCALE_NESTED_KEY}"]`;
 
 
 module.exports = {
@@ -20,7 +23,22 @@ module.exports = {
     global.window = mock;
   },
 
+  optionsStub(hash) {
+    return { hash: hash };
+  },
+
+  i18nNestedStub(value) {
+    return {
+      test: {
+        locale: { key: value || TEST_LOCALE_VALUE }
+      }
+    };
+  },
+
   TEST_LOCALE_KEY,
+  TEST_LOCALE_NESTED_KEY,
   TEST_LOCALE_VALUE,
-  TEST_LOCALE_ERROR
+  TEST_LOCALE_INTERPOLATED_PARAM,
+  TEST_LOCALE_ERROR,
+  TEST_LOCALE_NESTED_ERROR
 };
